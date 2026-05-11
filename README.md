43a1d8ab92bd5d384d316e059dd3d9d936ab13d2
yoel <57849691+YoelPilier@users.noreply.github.com>
2026-05-11 16:35:57 -0400

    chore(flakes): añadir flake y requisitos
    Se agrega configuración de entorno de desarrollo basada en Nix
    (flake.nix y flake.lock) y requirements.txt para dependencias Python.
    También se actualiza README con datos de autor y una cita. Facilita la
    reproducción de un entorno Jupyter/IA y el uso de fish en el shell.

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Material para la clase de Inteligencia Artificial 711. 
 
+## Yoel Andeyci Pilier Martinez
+
+### [yapmartinez@oymas.edu.do](mailto:yapmartinez@oymas.edu.do)
+
+> “Desprecia la teoría que no se pueda aplicar y la práctica que no tenga base científica.”
+
 ## Contenido
 
 - [Clase 1](./clase_1/clase-1.md)
---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Flake para un entorno Jupyter + IA + Fish 💻🐟";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    unstable,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      unpkgs = import unstable {inherit system;};
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "impurejupyterenv";
+
+        buildInputs = [
+          (pkgs.python3.withPackages (ps:
+            with ps; [
+              jupyter
+              ipykernel
+              notebook
+                pillow
+              matplotlib
+
+              datasets
+            ]))
+          pkgs.fish
+        ];
+
+        shellHook = ''
+          exec fish
+        '';
+      };
+    });
+}
